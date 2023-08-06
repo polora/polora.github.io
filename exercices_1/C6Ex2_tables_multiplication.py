96030037ca61e772ff8b8907e2b35acfdb78509f
polora <nann.ka@free.fr>
2023-08-06 08:39:57 +0200

    polora

--- exercices_1/C6Ex2_tables_multiplication.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-for x in range (1,11):
-    for y in range (1,11):
-        print ("{} x {} = {}".format(x,y,x*y))
-    print("\n")
--- pygame/cours/010_mouse_DEV.py
+++
@@ -1,4 +0,0 @@
-'''
-En cours de développement
-
-'''
